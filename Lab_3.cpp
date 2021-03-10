40abbd7d3eba8c07e80cde4639b6f8d0ab4bcdb5
Verrat-dash <79645169+Verrat-dash@users.noreply.github.com>
2021-03-10 20:35:33 +0600

    Update Lab_3.cpp

--- Lab_3.cpp
+++ Lab_3.cpp
@@ -1,3 +1,5 @@
+//Лаб 3, 3.1||Вялых Даша СБС-002 1п.
+
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -6,25 +8,26 @@ int main()
     setlocale(LC_ALL, "Russian");
     int i;
     double a[3];
-    double res=0;//переменная для хранения результата
-    cout << "Введите элементы массива a: ";
+    double res = 0;//переменная для хранения результата
+    int index = 0;//переменная для хранения результата
+    //cout << "Введите элементы массива a: ";
 
-    //        while(true)
-    //{
-    //    cout << "Введите элементы массива a: ";
-    //    cin >> a[3];
-    //        if(!cin)
-    //    {
-    //        cout << "не верно введен элемент массива a\n";
-    //        cin.clear();
-    //        while (cin.get() != '\n');
-    //    }
-    //    else break;
-    //}
+            while(index < 3) {
+        cout << "Введите элементы массива a: ";
+        cin >> a[index];
 
+            if(!cin) {
+            cout << "не верно введен элемент массива a\n";
+            cin.clear();
+            cin.ignore(32767,'\n');
+            //while (cin.get() != '\n');
+        } else {
+            index += 1;
+        }
+    }
 
-    for(int i = 0; i < 3; i++)
-        cin >> a[i];
+   // for(int i = 0; i < 3; i++)
+   //     cin >> a[i];
     for(int i = 0; i < 3; i++)
         res += a[i]*a[i];
         res = sqrt(res);
