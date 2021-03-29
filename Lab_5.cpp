486d57ee27e198b27c100aec8ebb6f632d9f9924
Verrat-dash <79645169+Verrat-dash@users.noreply.github.com>
2021-03-29 20:26:19 +0600

    Update Lab_5.cpp

--- Lab_5.cpp
+++ Lab_5.cpp
@@ -1,16 +1,73 @@
+#include <stdio.h>
 #include <iostream>
 #include <string>
-using namespace std;
 
-int main() {
-	string str;
-	cin >> str;
-	for(int i = 0 ; i < str.length(); i++)
+using std::wcin;
+using std::wcout;
+using std::wstring;
+
+int main()
+{
+    wcout << L"VVedite slovo: ";
+    wstring txt;
+    getline(std::wcin,txt);
+    for (size_t i = 0; i < txt.length(); ++i)
     {
-		if(str[i] == 'Z') str[i] = 'A';// не z,( можно начинать с а)
-		if(str[i] == '9') str[i] = '0';// не 9, ( можно начинать с 9)
-		else str[i]++;// тогда + 1 сдвиг буквы (a->b), то же самое и с числами
-	}
-	cout << str << endl;
-	return 0;
+        if (txt[i]==L'a')
+            wcout << L"01";
+        if (txt[i]==L'b')
+            wcout << L"02";
+        if (txt[i]==L'c')
+            wcout << L"03";
+        if (txt[i]==L'd')
+            wcout << L"04";
+        if (txt[i]==L'e')
+            wcout << L"05";
+        if (txt[i]==L'f')
+            wcout << L"06";
+        if (txt[i]==L'g')
+            wcout << L"07";
+        if (txt[i]==L'h')
+            wcout << L"08";
+        if (txt[i]==L'i')
+            wcout << L"09";
+        if (txt[i]==L'j')
+            wcout << L"10";
+        if (txt[i]==L'k')
+            wcout << L"11";
+        if (txt[i]==L'l')
+            wcout << L"12";
+        if (txt[i]==L'm')
+            wcout << L"13";
+        if (txt[i]==L'n')
+            wcout << L"14";
+        if (txt[i]==L'o')
+            wcout << L"15";
+        if (txt[i]==L'p')
+            wcout << L"16";
+        if (txt[i]==L'q')
+            wcout << L"17";
+        if (txt[i]==L'r')
+            wcout << L"18";
+        if (txt[i]==L's')
+            wcout << L"19";
+        if (txt[i]==L't')
+            wcout << L"20";
+        if (txt[i]==L'u')
+            wcout << L"21";
+        if (txt[i]==L'v')
+            wcout << L"22";
+        if (txt[i]==L'w')
+            wcout << L"23";
+        if (txt[i]==L'x')
+            wcout << L"24";
+        if (txt[i]==L'y')
+            wcout << L"25";
+        if (txt[i]==L'z')
+            wcout << L"26";
+        if (txt[i]==L' ')
+            wcout << L"00";
+    }
+    system("pause");
+    return 0;
 }
