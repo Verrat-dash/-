0f8766ce187b6b0a98c279504465fa1a85028abc
Verrat-dash <79645169+Verrat-dash@users.noreply.github.com>
2021-04-03 18:19:40 +0600

    Update Lab_5.cpp

--- Lab_5.cpp
+++ Lab_5.cpp
@@ -1,73 +1,28 @@
-#include <stdio.h>
 #include <iostream>
-#include <string>
-
-using std::wcin;
-using std::wcout;
-using std::wstring;
+#include <Windows.h>
+using namespace std;
 
 int main()
 {
-    wcout << L"VVedite slovo: ";
-    wstring txt;
-    getline(std::wcin,txt);
-    for (size_t i = 0; i < txt.length(); ++i)
+    setlocale(LC_ALL,"Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    char *str = new char[50];
+//    char r[] = " абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    char r[] = " abcdefghijklmnopqrstuvwxyz";
+    cout << "Введите слово английскими буквами : ";
+    gets(str);
+    cout << "То, что получилось(Ура!): ";
+    for(int i=0;i<strlen(str);i++)
     {
-        if (txt[i]==L'a')
-            wcout << L"01";
-        if (txt[i]==L'b')
-            wcout << L"02";
-        if (txt[i]==L'c')
-            wcout << L"03";
-        if (txt[i]==L'd')
-            wcout << L"04";
-        if (txt[i]==L'e')
-            wcout << L"05";
-        if (txt[i]==L'f')
-            wcout << L"06";
-        if (txt[i]==L'g')
-            wcout << L"07";
-        if (txt[i]==L'h')
-            wcout << L"08";
-        if (txt[i]==L'i')
-            wcout << L"09";
-        if (txt[i]==L'j')
-            wcout << L"10";
-        if (txt[i]==L'k')
-            wcout << L"11";
-        if (txt[i]==L'l')
-            wcout << L"12";
-        if (txt[i]==L'm')
-            wcout << L"13";
-        if (txt[i]==L'n')
-            wcout << L"14";
-        if (txt[i]==L'o')
-            wcout << L"15";
-        if (txt[i]==L'p')
-            wcout << L"16";
-        if (txt[i]==L'q')
-            wcout << L"17";
-        if (txt[i]==L'r')
-            wcout << L"18";
-        if (txt[i]==L's')
-            wcout << L"19";
-        if (txt[i]==L't')
-            wcout << L"20";
-        if (txt[i]==L'u')
-            wcout << L"21";
-        if (txt[i]==L'v')
-            wcout << L"22";
-        if (txt[i]==L'w')
-            wcout << L"23";
-        if (txt[i]==L'x')
-            wcout << L"24";
-        if (txt[i]==L'y')
-            wcout << L"25";
-        if (txt[i]==L'z')
-            wcout << L"26";
-        if (txt[i]==L' ')
-            wcout << L"00";
+        for(int j=0;j<strlen(r);j++)
+        {
+            if(str[i] == r[j])
+            {
+                cout << 0+j;
+            }
+        }
     }
+    cout << "\n";
     system("pause");
-    return 0;
 }
