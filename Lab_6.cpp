24db1c9ef16736b827b715cf81fb270d6c15f1b1
Verrat-dash <79645169+Verrat-dash@users.noreply.github.com>
2021-04-14 21:57:31 +0600

    Update Lab_6.cpp

--- Lab_6.cpp
+++ Lab_6.cpp
@@ -1,34 +1,64 @@
-#include<iostream>
-using namespace std;
-int t = 0;
-struct r {
-    r *p1;
-    r *p2;
-    int n;
-};
-r *k(int N) {
-    r *p;
-    p = new r;
-    t++;
-    p->n = t;
-    if (N > 1) {
-        p->p1 = k(N - 1);
-        p->p2 = k(N - 1);
-    }
-    return p;
-}
-int main() {
-    r *p;
-    p = k(4);
-    //cout << t << endl;
-    //cout << p << endl;
-    //cout <<"p->p2 = " << p->p2 << endl;
-    //cout <<"p->p1 = " << p->p1 << endl;
-    cout <<"p->p1->n = " << p->p1->n << endl;
-    cout << "p->p1->p2->n = " << p->p1->p2->n << endl;
-    cout << "p->p2->n = " << p->p2->n << endl;
-    cout << "p->p2->p1->n = " << p->p2->p1->n << endl;
+#include <iostream>
+#include <sstream>
 
-    system("pause");
-    return 0;
+
+struct Node {
+    int value = 0;
+    Node* left = nullptr;
+    Node* right = nullptr;
+};
+
+int generate_number() {
+    static int value = 1;
+    return value++;
+}
+
+void print(Node* top) {
+    std::cout << "\\*********Result structure*********//" << std::endl;
+    while (top->left != nullptr) {
+        std::cout << "  " << top->value << std::endl;
+        std::cout << top->left->value << "   " << top->right->value << std::endl;
+        top = top->left->left;
+    }
+    std::cout << "  " << top->value << std::endl;
+}
+
+Node* create_level(Node* top) {
+    Node* newTail = new Node();
+    Node* leftNode = new Node();
+    leftNode->value = generate_number();
+    Node* rightNode = new Node();
+    rightNode->value = generate_number();
+    top->left = leftNode;
+    top->right = rightNode;
+    leftNode->right = rightNode;
+    rightNode->left = leftNode;
+    leftNode->left = newTail;
+    rightNode->right = newTail;
+    newTail->value = generate_number();
+    return newTail;
+}
+
+
+int main() {
+    Node* root = new Node();
+    Node* tail = root;
+    int level_number = 0;
+    std::string user_input;
+    std::cout << "Enter number of level: ";
+    while (true) {
+        std::getline(std::cin, user_input);
+        try {
+            level_number = std::stoi(user_input);
+            break;
+        }
+        catch (...) {
+            std::cout << "Invalid nuber... try again: ";
+        }
+    }
+
+    for (int i = 0; i < level_number; i++) {
+        tail = create_level(tail);
+    }
+    print(root);
 }
